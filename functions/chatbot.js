3a10978b0f9a085d6c22d89adcb366b1a89f592a
Laurenz Frank <laurenz.frank@me.com>
2023-06-02 14:19:33 +0200

    chatbot

--- functions/chatbot.js
+++ functions/chatbot.js
@@ -1,5 +1,25 @@
-exports.handler = function(event, context, callback) {
+const { Configuration, OpenAIApi } = require( "openai");
+const configuration = new Configuration({
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+async function main() {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: "Hello world"}],
+    });
+    console.log(completion.data.choices[0].message);
+}
+
+
+
+exports.handler = async function(event, context, callback) {
     console.log(event.queryStringParameters.boopee);
+	const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: "Hello world"}],
+    });
+    console.log(completion.data.choices[0].message);
     callback(null,  {
 		statusCode: 200,
 		body: JSON.stringify({ message: 'hello' })
