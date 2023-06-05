6b2374e70af6e102c7cae6f4050d628d8087e00d
Laurenz Frank <laurenz.frank@me.com>
2023-06-05 09:35:43 +0200

    chatbot

--- Script/pillar.js
+++ Script/pillar.js
@@ -6,3 +6,56 @@ function getScrollProgress() {
   }
 
 //  document.getElementById("ScrollContainer").onscroll = function() {getScrollProgress()};
+
+/**
+ * opens tab of pillar html with an identifier
+ * @param {String} identifier 
+ * @param {HTMLElement} elem reference to itself
+ */
+function selectTab(identifier, elem) {
+    document.getElementsByClassName('rTH-element-active')[0].classList.toggle('rTH-element-active',false);
+    elem.classList.toggle('rTH-element-active',true);
+    document.getElementsByClassName('tabActive')[0].classList.toggle('tabActive', false);
+    document.getElementsByClassName(identifier)[0].classList.toggle('tabActive', true)
+}
+
+/**
+ * used to like message or thread
+ * @param {HTMLElement} elem reference to itself
+ */
+async function likeMessage(elem) {
+    elem.children[0].classList.toggle('messageliked');
+    if (elem.children[0].className.includes('messageliked')) {
+        await sleep(400);
+        var val = Number(elem.children[1].innerText) + 1;
+        elem.children[1].innerHTML = val;
+    } else {
+        var val = Number(elem.children[1].innerText) - 1;
+        elem.children[1].innerHTML = val;
+
+    }
+}
+
+/**
+ * leave current thread and opens thread list
+ * @param {String} reference 
+ */
+async function leaveThread(reference) {
+    var el = document.getElementsByName(reference)[0];
+    el.classList.toggle('tab-to-right', true);
+    // document.getElementsByName('ThreadList')[0].removeAttribute('style');
+    await sleep(300);
+    el.style.display = 'none';
+}
+
+/**
+ * 
+ * @param {String} reference to the thread that should be opened
+ */
+async function openThread(reference) {
+    var el = document.getElementsByName(reference)[0];
+    el.style.display = 'flex';
+    await sleep(10);
+    el.classList.toggle('tab-to-right', false);
+
+}
