d291e6b782e4079263cac6a67212003d4f57cf27
Laurenz Frank <laurenz.frank@me.com>
2023-06-30 13:53:57 +0200

    pillar

---
+++ Script/variables.js
@@ -0,0 +1,17 @@
+/**
+ * Structure: 
+ * Array-object:
+ *  {
+ *      directive: "CSRD",
+ *      numchecked: 2, // number of checklist items solved
+ *      numtotal: 30, //all checklist items
+ *      site: string // irrelevant
+    }
+ *
+ */
+var loadedsites = [{
+    directive: "CSRD",
+    numchecked: 2, // number of checklist items solved
+    numtotal: 30, //all checklist items
+    site: "string" // irrelevant
+    }];
