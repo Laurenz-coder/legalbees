0714d568acb223ba34497aff5d6bf2d6f21d380b
janfi123 <jan.fikentscher@gmail.com>
2023-07-05 20:09:12 +0200

    final product

--- Script/dashboard.js
+++ Script/dashboard.js
@@ -1,4 +1,6 @@
 
+import { loadedsites } from "./variables.js";
+
 // Get all the <td> elements with the class "mMR-warning-tag"
 const warningTags = document.querySelectorAll('.mMR-warning-tag');
 
@@ -18,28 +20,32 @@ warningTags.forEach((tag) => {
 });
 
 const col = [
-  
-  
+
+
   getComputedStyle(document.documentElement)
     .getPropertyValue('--red-color'),
-    getComputedStyle(document.documentElement)
+  getComputedStyle(document.documentElement)
     .getPropertyValue('--warning-color'),
   getComputedStyle(document.documentElement)
     .getPropertyValue('--main-color')
-  ];
+];
+
+
+
+
 
 
 const data = {
-  labels: ['bad', 'medium', 'Wed'],
+  labels: ['bad', 'medium', 'good'],
   datasets: [{
     label: 'Weekly Sales',
-    data: [50,30,20],
+    data: [50, 30, 20],
     backgroundColor: col,
     borderWidth: 1,
     circumference: 180,
     rotation: 270,
     cutout: "85%",
-    needleValue: 75,
+    needleValue: updateChart(),
     borderRadius: 30
   }]
 };
@@ -50,7 +56,7 @@ const gaugeNeedle = {
     const { ctx, data } = chart;
 
     ctx.save();
-    console.log(chart.getDatasetMeta(0).data);
+
     const xCenter = chart.getDatasetMeta(0).data[0].x;
     const yCenter = chart.getDatasetMeta(0).data[0].y;
     const outRad = chart.getDatasetMeta(0).data[0].outerRadius;
@@ -128,7 +134,7 @@ const gaugeFlowMeter = {
 // config 
 const config = {
   type: 'doughnut',
-  data,
+  data: data,
   options: {
     layout: {
       padding: {
@@ -149,6 +155,9 @@ const config = {
   plugins: [gaugeNeedle, gaugeFlowMeter]
 };
 
+
+
+
 // render init block
 const myChart = new Chart(
   document.getElementById('doughnut-chart').getContext("2d"),
@@ -156,25 +165,197 @@ const myChart = new Chart(
 );
 
 
-document.addEventListener('DOMContentLoaded', function() {
-  expandPillar();
-});
+//////////////////////// setup 
+
+
+const data2 = {
+  labels: ['filled', ''],
+  datasets: [{
+    label: 'Directives fullfilled',
+    data: [updateChart(), 100-updateChart()],
+    fill: false,
+    backgroundColor: [
+      getComputedStyle(document.documentElement)
+        .getPropertyValue('--main-color'),
+      'transparent'
+    ],
+    borderColor: [
+      getComputedStyle(document.documentElement)
+        .getPropertyValue('--main-color'),
+      'transparent'
+    ],
+    borderWidth: 1,
+    cutout: '75%',
+    borderRadius: 25,
+  
+  
+  }]
+};
+
+const gaugeFlowMeter2 = {
+  id: "gaugeFlowMeter2",
+  afterDatasetsDraw(chart, args, pluginOptions) {
+    const { ctx, data } = chart;
+
+    ctx.save()
+    const xCenter = chart.getDatasetMeta(0).data[0].x;
+    const yCenter = chart.getDatasetMeta(0).data[0].y;
+        // flowmeter
+
+    ctx.font = "bold 15px Inter";
+    ctx.fillStyle = "black";
+    ctx.textAlign = "center";
+    ctx.textBaseline = 'middle'
+    ctx.fillText(`${updateChart().toFixed(0)}%`, xCenter, yCenter );
+
+    console.log(ctx);
+
+  }
+}
+
+
+
+
+
+
+// config 
+const configuration = {
+  type: 'doughnut',
+  data: data2,
+  options: {
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        display: false
+      },
+      tooltip: {
+        enabled: false
+      }
+    }
+  },
+  plugins: [gaugeFlowMeter2]
+};
+
+
+
+// render init block
+
+const directive_chart = new Chart(
+  document.getElementById("directive-chart").getContext("2d"),
+  configuration
+);
+//////////////////////////////
+
+
+function updateChart() {
+  var totalDirectives = loadedsites.length;
+  let totalChecked = 0;
+  let totalNumTotal = 0;
+  for (let i = 0; i < loadedsites.length; i++) {
+    totalChecked += loadedsites[i].numchecked;
+    totalNumTotal += loadedsites[i].numtotal;
+
+  }
+
+
+  return (totalChecked / totalNumTotal) * 100;
+
+}
+
+
+
+
+
+
+
 
 
 function expandPillar() {
   var gridContainer = document.getElementById("active");
   var targetElements = Array.from(document.getElementsByClassName("sub-pillar"));
-  console.log("test;");
+
   gridContainer.addEventListener('click', () => {
     targetElements.forEach(element => {
       element.classList.toggle("inactive");
-      
+
     });
   });
-
-
-
-    
-  
-  
 }
+
+function fillSubPillar() {
+  const el = document.getElementById("insights-pillars-container");
+
+  for (let i = 0; i < loadedsites.length; i++) {
+    let dir = loadedsites[i];
+    let div = document.createElement("div");
+    div.style.gridArea = `div${i + 4}`;
+    div.classList.add("box-layout", "sub-pillar", "inactive", `div${i + 4}`);
+
+    let span = document.createElement("span");
+    span.classList.add("material-symbols-outlined");
+
+    let mMIEContentArea = document.createElement("div");
+    mMIEContentArea.classList.add("mMIE-content-area");
+
+    let mMIECTextArea = document.createElement("div");
+    mMIECTextArea.classList.add("mMIEC-text-area");
+
+    let h3 = document.createElement("h3");
+    h3.textContent = dir.directive;
+
+    let h1 = document.createElement("h1");
+    h1.textContent = `${dir.numchecked} / ${dir.numtotal}`;
+
+    // Add appropriate symbol based on directive
+    if (dir.directive === "CSRD") {
+      span.textContent = "spa";
+    } else {
+      span.textContent = "filter_list";
+    }
+
+    // Append the elements in the required hierarchy
+    mMIECTextArea.appendChild(h3);
+    mMIECTextArea.appendChild(h1);
+    mMIEContentArea.appendChild(span);
+    mMIEContentArea.appendChild(mMIECTextArea);
+    div.appendChild(mMIEContentArea);
+    el.appendChild(div);
+  }
+}
+
+
+function updateDirectives() {
+  var totalDirectives = loadedsites[0].length;
+  let totalChecked = 0;
+  let totalNumTotal = 0;
+  for (let i = 0; i < loadedsites.length; i++) {
+    totalChecked += loadedsites[i].numchecked;
+    totalNumTotal += loadedsites[i].numtotal;
+
+  }
+  const element = document.getElementsByClassName("mMIEC-text-area");
+ 
+  console.log(element);
+  element[0].innerHTML = `<h3>Directive</h3><h1>${totalChecked} / ${totalNumTotal}</h1>`;
+}
+
+function updateDisplayValue(){
+  // Create mMIEGP-number div
+let mMIEGPNumberDiv = document.createElement("div");
+mMIEGPNumberDiv.classList.add("mMIEGP-number");
+
+let p = document.createElement("p");
+p.textContent = updateChart();
+
+mMIEGPNumberDiv.appendChild(p);
+
+}
+
+
+
+
+updateDirectives();
+
+fillSubPillar();
+
+expandPillar();
--- Script/variables.js
+++ Script/variables.js
@@ -11,7 +11,24 @@
  */
 var loadedsites = [{
     directive: "CSRD",
-    numchecked: 2, // number of checklist items solved
+    numchecked: 30, // number of checklist items solved
     numtotal: 30, //all checklist items
     site: "string" // irrelevant
-    }];
+    },{
+      directive: "EU Taxonomy",
+      numchecked: 15, // number of checklist items solved
+      numtotal: 20, //all checklist items
+      site: "string" // irrelevant
+      },{
+         directive: "LkSG",
+         numchecked: 10, // number of checklist items solved
+         numtotal: 15, //all checklist items
+         site: "string" // irrelevant
+         },{
+            directive: "ESRS",
+            numchecked: 5, // number of checklist items solved
+            numtotal: 5, //all checklist items
+            site: "string" // irrelevant
+            }];
+    
+export {loadedsites};
